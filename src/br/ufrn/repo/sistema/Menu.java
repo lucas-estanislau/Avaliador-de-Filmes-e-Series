6221335399769ee054b6f1b27a46c4df38117c57
Juvam Rodrigues <juvamrodrigues17@gmail.com>
2025-12-05 11:48:01 -0300

    Criação de anotação: InfoAuto, FuncionamentoClasse e FuncionamentoMetodo. Fiz também a leitura e impressão desses metadados de InfoAutor via reflexão (requisito da atividade).

--- src/br/ufrn/repo/sistema/Menu.java
+++ src/br/ufrn/repo/sistema/Menu.java
@@ -2,127 +2,144 @@ package br.ufrn.repo.sistema;
 
 import java.util.Scanner;
 
+import br.ufrn.repo.annotations.FuncionamentoMetodo;
+import br.ufrn.repo.annotations.InfoAutor;
+import br.ufrn.repo.avaliacao.Avaliacao;
+
 public class Menu {
-    private Scanner sc;
-    private boolean verificadorAvaliacao;
-    private boolean verificadorVisualizar;
+	private Scanner sc;
+	private boolean verificadorAvaliacao;
+	private boolean verificadorVisualizar;
 
-    public Menu() {
-        this.sc = new Scanner(System.in);
-        this.verificadorAvaliacao = true;
-        this.verificadorVisualizar = false;
-    }
+	public Menu() {
+		lerAnotacaoInfoAutor();
+		this.sc = new Scanner(System.in);
+		this.verificadorAvaliacao = true;
+		this.verificadorVisualizar = false;
+	}
 
-    public void exibir() {
-        while(verificadorAvaliacao) {
-            MenuAvaliacao();
-            int opcao = lerOpcao();
-            switchCaseAvaliacao(opcao);
-        }
-        sc.close();
-    }
+	public void exibir() {
+		while (verificadorAvaliacao) {
+			MenuAvaliacao();
+			int opcao = lerOpcao();
+			switchCaseAvaliacao(opcao);
+		}
+		sc.close();
+	}
+	
+	private void MenuAvaliacao() {
+		System.out.println("\n========== MENU ==========");
+		System.out.println("1. Adicionar Avaliação");
+		System.out.println("2. Visualizar Avaliação");
+		System.out.println("3. Editar Avaliação");
+		System.out.println("4. Deletar Avaliação");
+		System.out.println("0. Sair");
+		System.out.print("Escolha uma opção: ");
+	}
 
-    private void MenuAvaliacao() {
-        System.out.println("\n========== MENU ==========");
-        System.out.println("1. Adicionar Avaliação");
-        System.out.println("2. Visualizar Avaliação");
-        System.out.println("3. Editar Avaliação");
-        System.out.println("4. Deletar Avaliação");
-        System.out.println("0. Sair");
-        System.out.print("Escolha uma opção: ");
-    }
+	private void MenuVisualizarAvaliacao() {
+		System.out.println("\n========== MENU ==========");
+		System.out.println("1. Visualizar 5 filmes mais recentes");
+		System.out.println("2. Visualizar 5 séries mais recentes");
+		System.out.println("3. Voltar ao menu anterior");
+		System.out.print("Escolha uma opção: ");
+	}
 
-    private void MenuVisualizarAvaliacao() {
-        System.out.println("\n========== MENU ==========");
-        System.out.println("1. Visualizar 5 filmes mais recentes");
-        System.out.println("2. Visualizar 5 séries mais recentes");
-        System.out.println("3. Voltar ao menu anterior");
-        System.out.print("Escolha uma opção: ");
-    }
+	private int lerOpcao() {
+		try {
+			return Integer.parseInt(sc.nextLine());
+		} catch (NumberFormatException e) {
+			return -1;
+		}
+	}
 
-    private int lerOpcao() {
-        try {
-            return Integer.parseInt(sc.nextLine());
-        } catch (NumberFormatException e) {
-            return -1;
-        }
-    }
+	private void switchCaseAvaliacao(int opcao) {
+		switch (opcao) {
+		case 1:
+			adicionarAvaliacao();
+			break;
+		case 2:
+			this.verificadorVisualizar = true;
+			while (verificadorVisualizar) {
+				MenuVisualizarAvaliacao();
+				opcao = lerOpcao();
+				switchCaseVisualizarAvaliacao(opcao);
+			}
+			break;
+		case 3:
+			editarAvaliacao();
+			break;
+		case 4:
+			deletarAvaliacao();
+			break;
+		case 0:
+			encerrar();
+			break;
+		default:
+			System.out.println("Opção inválida! Tente novamente.");
+		}
+	}
 
-    private void switchCaseAvaliacao(int opcao) {
-        switch(opcao) {
-            case 1:
-                adicionarAvaliacao();
-                break;
-            case 2:
-                this.verificadorVisualizar = true;
-                while(verificadorVisualizar) {
-                    MenuVisualizarAvaliacao();
-                    opcao = lerOpcao();
-                    switchCaseVisualizarAvaliacao(opcao);
-                }
-                break;
-            case 3:
-                editarAvaliacao();
-                break;
-            case 4:
-                deletarAvaliacao();
-                break;
-            case 0:
-                encerrar();
-                break;
-            default:
-                System.out.println("Opção inválida! Tente novamente.");
-        }
-    }
+	private void switchCaseVisualizarAvaliacao(int opcao) {
+		switch (opcao) {
+		case 1:
+			exibirFilmes();
+			break;
+		case 2:
+			exibirSeries();
+			break;
+		case 3:
+			this.verificadorVisualizar = false;
+			break;
+		default:
+			System.out.println("Opção inválida! Tente novamente.");
+		}
+	}
 
-    private void switchCaseVisualizarAvaliacao(int opcao) {
-        switch(opcao) {
-            case 1:
-                exibirFilmes();
-                break;
-            case 2:
-                exibirSeries();
-                break;
-            case 3:
-                this.verificadorVisualizar = false;
-                break;
-            default:
-                System.out.println("Opção inválida! Tente novamente.");
-        }
-    }
+	// Métodos do menu de avaliação
+	private void adicionarAvaliacao() {
+		System.out.println("========== ADICIONAR AVALIAÇÃO ==========");
+		// Adicionar o metodo de adicionar avaliação
+	}
 
-    // Métodos do menu de avaliação
-    private void adicionarAvaliacao() {
-        System.out.println("========== ADICIONAR AVALIAÇÃO ==========");
-        // Adicionar o metodo de adicionar avaliação
-    }
+	private void visualizarAvaliacao() {
+		System.out.println("========== VISUALIZAR AVALIAÇÕES ==========");
+		// Implementação
+	}
 
-    private void visualizarAvaliacao() {
-        System.out.println("========== VISUALIZAR AVALIAÇÕES ==========");
-        // Implementação
-    }
+	private void editarAvaliacao() {
+		System.out.println("========== EDITAR AVALIAÇÃO ==========");
+		// Implementação
+	}
 
-    private void editarAvaliacao() {
-        System.out.println("========== EDITAR AVALIAÇÃO ==========");
-        // Implementação
-    }
+	private void deletarAvaliacao() {
+		System.out.println("========== DELETAR AVALIAÇÃO ==========");
+		// Implementação
+	}
 
-    private void deletarAvaliacao() {
-        System.out.println("========== DELETAR AVALIAÇÃO ==========");
-        // Implementação
-    }
+	private void encerrar() {
+		System.out.println("Encerrando programa...");
+		this.verificadorAvaliacao = false;
+	}
 
-    private void encerrar() {
-        System.out.println("Encerrando programa...");
-        this.verificadorAvaliacao = false;
-    }
+	// Métodos do menu de visualizar avaliações
+	private void exibirFilmes() {
+		System.out.println("========== FILMES ==========");
+	}
 
-    // Métodos do menu de visualizar avaliações
-    private void exibirFilmes() {
-        System.out.println("========== FILMES ==========");
-    }
-
-    private void exibirSeries() {
-        System.out.println("========== SÉRIES ==========");
-    }
+	private void exibirSeries() {
+		System.out.println("========== SÉRIES ==========");
+	}
+	
+	@FuncionamentoMetodo(funcionamento="Ler os elementos da anotação InfoAutor via reflexão e imprime-os.")
+	private void lerAnotacaoInfoAutor() {
+		Class<Avaliacao> clazz = Avaliacao.class;
+		InfoAutor anot = clazz.getAnnotation(InfoAutor.class);
+		imprimirAnotacao(anot);
+	}
+	private void imprimirAnotacao(InfoAutor anot) {
+		System.out.println("Autores: " + anot.nome());
+		System.out.println("Data: " + anot.data());
+		System.out.println("Versão projeto: " + anot.versaoProjeto());
+	}
 }
